9f60721850c74e77133c442fb1ead88e811793a9
Daily Stats <jan.loeffler@webpros.com>
2023-02-01 10:28:50 +0100

    trying some changes on imports to please PyPi

--- cli360monitoring/cli360monitoring.py
+++ cli360monitoring/cli360monitoring.py
@@ -4,13 +4,14 @@ import os
 import argparse
 import json
 
-from lib.monitoringconfig import MonitoringConfig
-from lib.contacts import Contacts
-from lib.servers import Servers
-from lib.sites import Sites
-from lib.usertokens import UserTokens
+from .lib.monitoringconfig import MonitoringConfig
+from .lib.contacts import Contacts
+from .lib.servers import Servers
+from .lib.sites import Sites
+from .lib.usertokens import UserTokens
+from .lib.statistics import Statistics
 
-__version__ = '1.0.4'
+__version__ = '1.0.5'
 
 monitoringconfig = MonitoringConfig()
 cli = argparse.ArgumentParser(description='CLI for 360 Monitoring')
@@ -27,6 +28,12 @@ def config(args):
 
     else:
         monitoringconfig.print()
+
+def statistics(args):
+    """Sub command for statistics"""
+    statistics = Statistics(monitoringconfig)
+    statistics.format = args.format
+    statistics.print()
 
 def servers(args):
     """Sub command for servers"""
@@ -168,16 +175,17 @@ def performCLI():
 
     cli_config = subparsers.add_parser('config', help='configure connection to 360 Monitoring account')
     cli_config.set_defaults(func=config)
-    cli_config.add_argument('-a', '--api-key', help='specify your API KEY for 360 Monitoring')
+    cli_config.add_argument('-a', '--api-key', metavar='key', help='specify your API KEY for 360 Monitoring')
     cli_config.add_argument('-p', '--print', action='store_true', help='print your current settings for 360 Monitoring')
     cli_config.add_argument('-s', '--save', action='store_true', help='save your current settings for 360 Monitoring to the default ini file')
 
     cli_servers = subparsers.add_parser('servers', help='list and manage all monitored servers')
     cli_servers.set_defaults(func=servers)
-    cli_servers.add_argument('-a', '--add', action='store_true', help='explain how to add a server monitor')
-    cli_servers.add_argument('-g', '--get', nargs='+', help='servers to print')
-    cli_servers.add_argument('-l', '--list', action='store_true', help='list all monitored servers')
-    cli_servers.add_argument('-r', '--remove', action='store_true', help='explain how to add a server monitor')
+    group_servers = cli_servers.add_mutually_exclusive_group()
+    group_servers.add_argument('-a', '--add', action='store_true', help='explain how to add a server monitor')
+    group_servers.add_argument('-g', '--get', nargs='+', metavar='name', help='servers to print')
+    group_servers.add_argument('-l', '--list', action='store_true', help='list all monitored servers')
+    group_servers.add_argument('-r', '--remove', action='store_true', help='explain how to add a server monitor')
 
     cli_servers.add_argument('--format', choices=['json', 'csv', 'table'], default='table', help='output format for the data')
     cli_servers.add_argument('--json', action='store_const', const='json', dest='format', help='print data in JSON format')
@@ -186,14 +194,16 @@ def performCLI():
 
     cli_sites = subparsers.add_parser('sites', help='list and manage all monitored websites')
     cli_sites.set_defaults(func=sites)
-    cli_sites.add_argument('-a', '--add', nargs='+', help='URL(s) to add')
-    cli_sites.add_argument('-g', '--get', nargs='+', help='URL(s) to print')
-    cli_sites.add_argument('-l', '--list', action='store_true', help='list all monitored urls')
-    cli_sites.add_argument('-r', '--remove', nargs='+', help='URL(s) to remove')
-    cli_sites.add_argument('-f', '--add-from-file', help='file containing one URL per line to add')
+    group_sites = cli_sites.add_mutually_exclusive_group()
+    group_sites.add_argument('-a', '--add', nargs='+', metavar='url', help='URL(s) to add')
+    group_sites.add_argument('-g', '--get', nargs='+', metavar='url', help='URL(s) to print')
+    group_sites.add_argument('-l', '--list', action='store_true', help='list all monitored urls')
+    group_sites.add_argument('-r', '--remove', nargs='+', metavar='url', help='URL(s) to remove')
+    group_sites.add_argument('-f', '--add-from-file', metavar='filepath', help='file containing one URL per line to add')
 
-    cli_sites.add_argument('--name', nargs='?', default='', help='give the new monitor a specific name. Otherwise the url is used as name.')
-    cli_sites.add_argument('--protocol', nargs='?', default='https', help='specify a different protocol than https')
+    cli_sites.add_argument('--name', nargs='?', default='', metavar='name', help='give the new monitor a specific name. Otherwise the url is used as name.')
+    cli_sites.add_argument('--protocol', nargs='?', default='https', metavar='protocol', help='specify a different protocol than https')
+#    cli_sites.add_argument('--port', nargs='?', default=443, type=int, metavar='port', help='specify a different port than 443')
     cli_sites.add_argument('--force', action='store_true', help='add new monitor even if already exists')
 
     cli_sites.add_argument('--format', choices=['json', 'csv', 'table'], default='table', help='output format for the data')
@@ -203,14 +213,15 @@ def performCLI():
 
     cli_contacts = subparsers.add_parser('contacts', help='list and manage all contacts')
     cli_contacts.set_defaults(func=contacts)
-    cli_contacts.add_argument('-a', '--add', nargs='+', help='contact to add')
-    cli_contacts.add_argument('-g', '--get', nargs='+', help='contact to print')
-    cli_contacts.add_argument('-l', '--list', action='store_true', help='list all contacts')
-    cli_contacts.add_argument('-r', '--remove', nargs='+', help='contact to remove')
-    cli_contacts.add_argument('-f', '--add-from-file', help='file containing one contact per line to add')
+    group_contacts = cli_contacts.add_mutually_exclusive_group()
+    group_contacts.add_argument('-a', '--add', nargs='+', metavar='name', help='contact to add')
+    group_contacts.add_argument('-g', '--get', nargs='+', metavar='name', help='contact to print')
+    group_contacts.add_argument('-l', '--list', action='store_true', help='list all contacts')
+    group_contacts.add_argument('-r', '--remove', nargs='+', metavar='name', help='contact to remove')
+    group_contacts.add_argument('-f', '--add-from-file', metavar='filepath', help='file containing one contact per line to add')
 
-    cli_contacts.add_argument('--email', nargs='?', default='', help='email address for the new contact.')
-    cli_contacts.add_argument('--sms', nargs='?', default='', help='mobile phone number for the new contact.')
+    cli_contacts.add_argument('--email', nargs='?', default='', metavar='email', help='email address for the new contact.')
+    cli_contacts.add_argument('--sms', nargs='?', default='', metavar='phone_number', help='mobile phone number for the new contact.')
 
     cli_contacts.add_argument('--format', choices=['json', 'csv', 'table'], default='table', help='output format for the data')
     cli_contacts.add_argument('--json', action='store_const', const='json', dest='format', help='print data in JSON format')
@@ -219,20 +230,29 @@ def performCLI():
 
     cli_usertokens = subparsers.add_parser('usertokens', help='list or create usertokens')
     cli_usertokens.set_defaults(func=usertokens)
-    cli_usertokens.add_argument('-c', '--create', action='store_true', help='create new user token')
-    cli_usertokens.add_argument('-g', '--get', nargs='+', help='usertoken to print')
-    cli_usertokens.add_argument('-l', '--list', action='store_true', help='list all usertokens')
+    group_contacts = cli_usertokens.add_mutually_exclusive_group()
+    group_contacts.add_argument('-c', '--create', action='store_true', help='create new user token')
+    group_contacts.add_argument('-g', '--get', nargs='+', help='usertoken to print')
+    group_contacts.add_argument('-l', '--list', action='store_true', help='list all usertokens')
 
     cli_usertokens.add_argument('--format', choices=['json', 'csv', 'table'], default='table', help='output format for the data')
     cli_usertokens.add_argument('--json', action='store_const', const='json', dest='format', help='print data in JSON format')
     cli_usertokens.add_argument('--csv', action='store_const', const='csv', dest='format', help='print data in CSV format')
     cli_usertokens.add_argument('--table', action='store_const', const='table', dest='format', help='print data as ASCII table')
 
+    cli_statistics = subparsers.add_parser('statistics', help='print statistics')
+    cli_statistics.set_defaults(func=statistics)
+    cli_statistics.add_argument('-p', '--print', action='store_true', help='print statistics for 360 Monitoring')
+    cli_statistics.add_argument('--format', choices=['csv', 'table'], default='table', help='output format for the data')
+    cli_statistics.add_argument('--csv', action='store_const', const='csv', dest='format', help='print data in CSV format')
+    cli_statistics.add_argument('--table', action='store_const', const='table', dest='format', help='print data as ASCII table')
+
     cli_subcommands['config'] = cli_config
     cli_subcommands['contacts'] = cli_contacts
     cli_subcommands['servers'] = cli_servers
     cli_subcommands['sites'] = cli_sites
     cli_subcommands['usertokens'] = cli_usertokens
+    cli_subcommands['statistics'] = cli_statistics
 
     args = cli.parse_args()
     if args.subparser == None:
---
+++ cli360monitoring/lib/statistics.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+from prettytable import PrettyTable
+
+from .monitoringconfig import MonitoringConfig
+from .servers import Servers
+from .sites import Sites
+from .contacts import Contacts
+from .usertokens import UserTokens
+from .functions import printError, printWarn
+from .bcolors import bcolors
+
+class Statistics(object):
+
+    def __init__(self, config):
+        self.config = config
+        self.statistics = None
+        self.format = 'table'
+
+        self.table = PrettyTable()
+        self.table.field_names = ['Value', 'Metric']
+        self.table.align['Value'] = 'r'
+        self.table.align['Metric'] = 'l'
+
+    def print(self):
+        """Iterate through all assets and print statistics"""
+
+        self.printHeader()
+
+        servers = Servers(self.config)
+        if servers.fetchData():
+            self.printAsset('Servers', len(servers.servers))
+
+        sites = Sites(self.config)
+        if sites.fetchData():
+            sum_uptime = 0
+            sum_ttfb = 0
+            num_monitors = len(sites.monitors)
+            self.printAsset('Sites', len(sites.monitors))
+
+            for monitor in sites.monitors:
+                uptime_percentage = float(monitor['uptime_percentage'])
+
+                if 'last_check' in monitor and 'ttfb' in monitor['last_check']:
+                    ttfb = float(monitor['last_check']['ttfb'])
+                    sum_uptime = sum_uptime + uptime_percentage
+                    sum_ttfb = sum_ttfb + ttfb
+
+            avg_uptime = sum_uptime / num_monitors
+            avg_ttfb = sum_ttfb / num_monitors
+
+            if avg_uptime <= float(self.config.threshold_uptime):
+                uptime_percentage_text = f"{bcolors.FAIL}" + "{:.4f}".format(avg_uptime) + f"{bcolors.ENDC}"
+            else:
+                uptime_percentage_text = "{:.4f}".format(avg_uptime)
+
+            if avg_ttfb >= float(self.config.threshold_ttfb):
+                ttfb_text = f"{bcolors.FAIL}" + "{:.2f}".format(avg_ttfb) + f"{bcolors.ENDC}"
+            else:
+                ttfb_text = "{:.2f}".format(avg_ttfb)
+
+            self.printAsset('% avg uptime of all sites', uptime_percentage_text)
+            self.printAsset('sec avg ttfb of all sites', ttfb_text)
+
+        contacts = Contacts(self.config)
+        if contacts.fetchData():
+            self.printAsset('Contacts', len(contacts.contacts))
+
+        usertokens = UserTokens(self.config)
+        if usertokens.fetchData():
+            self.printAsset('User Tokens', len(usertokens.usertokens))
+
+        self.printFooter()
+
+    def printHeader(self):
+        """Print CSV header if CSV format requested"""
+
+        if (self.format == 'csv'):
+            print('metric;value')
+
+    def printFooter(self):
+        """Print table if table format requested"""
+
+        if (self.format == 'table'):
+            print(self.table)
+
+    def printAsset(self, asset: str, value):
+        """Print the data of the specified web monitor"""
+
+        if (self.format == 'table'):
+            self.table.add_row([value, asset])
+
+        elif (self.format == 'csv'):
+            print(f"{asset};{value}")
